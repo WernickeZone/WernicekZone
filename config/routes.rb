0ad561d9bbbb1c65d18512e3e6a30690a0a60a9a
raffin-j <raffin_j@etna-alternance.net>
2015-03-25 14:45:25 +0100

    nouveau commit initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :products
-root :to => 'products#index'
+
+resources :landing
+root :to => 'landing#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
