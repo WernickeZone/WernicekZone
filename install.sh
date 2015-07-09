4a8f43ffcd00ede839c63357011d445499b9c773
raffin-j <raffin_j@etna-alternance.net>
2015-07-09 09:40:09 +0200

    script de déploiment

--- install.sh
+++ install.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-aptitude update
-aptitude upgrade -y
-aptitude install -y ruby2.1 ruby2.1-dev nodejs nodejs-dev ruby-rails
+echo "deb http://ftp.fr.debian.org/debian/ sid main\ndeb-src http://ftp.fr.debian.org/debian/ sid main" > /etc/apt/sources.list
+
+apt-get update
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt-get install -y aptitude
+
+aptitude install -y ruby ruby-dev nodejs nodejs-dev ruby-rails git
 aptitude install -y openjdk-7-jdk openjdk-7-jre-lib
-aptitude install -y ImageMagick libmagickcore-dev libmagick-dev zlib1g-dev python-pkgconfig ruby-rmagick
-gem install bundler
-gem install matrix
-gem install rake –v 10.3.2
-gem install tf-idf-similarity
-gem install jrb
-gem install stanford-core-nlp
-gem install treat
-git clone https://github.com/WernickeZone/WernickeZone.git
+aptitude install -y ImageMagick libmagickcore-dev libmagick-dev zlib1g-dev python-pkgconfig ruby-rmagick aspell aspell-fr libmagick++-dev
+
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
+
+gem install bundler matrix tf-idf-similarity jrb stanford-core-nlp treat railties
+gem install rake -v 10.3.2
+
+git clone https://github.com/FloGro/WernickeZone.git
+
 cd WernickeZone
 bundle install
+bin/rake db:migrate RAILS_ENV=development
 rails s -d -p 80
