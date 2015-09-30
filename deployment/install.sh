69320901c390acb9c288606cf8c2dbe2e81d3506
raffin-j <raffin_j@etna-alternance.net>
2015-09-30 13:37:14 +0200

    correction

--- deployment/install.sh
+++ deployment/install.sh
@@ -9,12 +9,12 @@ apt-get install -y aptitude
 
 aptitude install -y ruby ruby-dev nodejs nodejs-dev ruby-rails git
 aptitude install -y openjdk-7-jdk openjdk-7-jre-lib
-aptitude install -y ImageMagick libmagickcore-dev libmagick-dev zlib1g-dev python-pkgconfig ruby-rmagick aspell aspell-fr libmagick++-dev libleptonica-dev tesseract-ocr-fra libtesseract-dev
+aptitude install -y ImageMagick libmagickcore-dev libmagick++-dev zlib1g-dev python-pkgconfig ruby-rmagick aspell aspell-fr libmagick++-dev libleptonica-dev tesseract-ocr-fra libtesseract-dev
 
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
 
 gem install bundler matrix tf-idf-similarity jrb stanford-core-nlp treat railties tesseract
-gem install rake -v 10.3.2
+gem install rake -v 10.4.2
 
 git clone https://github.com/FloGro/WernickeZone.git
 
