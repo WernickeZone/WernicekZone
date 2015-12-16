09b9478ac7114c115ded0ac8e10bdb32c1c07078
FloGro <grogne_f@etna-alternance.net>
2015-12-16 18:41:56 +0100

    Update install.sh

--- deployment/install.sh
+++ deployment/install.sh
@@ -7,16 +7,16 @@ apt-get upgrade -y
 apt-get dist-upgrade -y
 apt-get install -y aptitude
 
-aptitude install -y ruby2.1 ruby2.1-dev nodejs nodejs-dev ruby-rails git
-aptitude install -y openjdk-7-jdk openjdk-7-jre-lib
-aptitude install -y ImageMagick libmagickcore-dev libmagick++-dev zlib1g-dev python-pkgconfig ruby-rmagick aspell aspell-fr libmagick++-dev libleptonica-dev tesseract-ocr-fra libtesseract-dev
-aptitude install -y verbiste
+aptitude install -y ruby2.1 ruby2.1-dev nodejs nodejs-dev git
+aptitude install -y openjdk-7-jdk openjdk-7-jre-lib ruby-rails 
+aptitude install -y ImageMagick libmagickcore-dev libmagick++-dev zlib1g-dev python-pkgconfig ruby-rmagick aspell aspell-fr libmagick++-dev 
+aptitude install -y verbiste libleptonica-dev tesseract-ocr tesseract-ocr-fra libtesseract-dev libaspell-dev sqlite3 libsqlite3-dev
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
 
 gem install bundler matrix tf-idf-similarity jrb stanford-core-nlp treat railties tesseract
 gem install rake -v 10.4.2
 
-git clone https://github.com/Wernicke/WernickeZone.git
+git clone https://github.com/WernickeZone/WernickeZone.git
 
 cd WernickeZone
 bundle install
