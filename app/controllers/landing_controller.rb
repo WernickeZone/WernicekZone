4c9dd98f7a073c2da52921a692e49fae77797b18
FloGro <grogne_f@etna-alternance.net>
2016-01-11 15:28:45 +0100

    Page d'accueil, mise-à-jour
    - Report des changements de AS sur les liens vers les réseaux sociaux
    (V)
    - Ajout du lien vers la vidéo youtube WZ (V)
    - Inscription à la newsletter (MVC)
    - Formulaire de contact (MVC)

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,21 @@
 class LandingController < ApplicationController
   def index
   end
+
+  def create
+    if !params[:newsletter].nil?
+      if Newsletter.find_by(email: params[:newsletter][:email].downcase).nil?
+        @newsletter = Newsletter.new
+        @newsletter.email = params[:newsletter][:email].downcase
+        @newsletter.save!
+      end
+    elsif !params[:contacts].nil?
+      @contact = Contact.new
+      @contact.nom = params[:contact][:nom]
+      @contact.email = params[:contact][:email].downcase
+      @contact.message params[:contact][:message]
+      @contact.save!
+    end
+    redirect_to :action => :index
+  end
 end
